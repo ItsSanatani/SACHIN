be8cc2d7dd796b8d54bf16a5dfc14c4f00695ca1
ItsSanatani <sachinxmeena2000@gmail.com>
2025-03-23 16:45:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Bot configuration
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-API_ID = YOUR_API_ID
-API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "7656369802:AAGdlo88cewouuiviq-eHoRHdxj_Ktji3To"
+API_ID = 28795512
+API_HASH = "c17e4eb6d994c9892b8a8b6bfea4042a"
 
-# List of session strings
 SESSION_STRINGS = [
-    "SESSION_STRING_1",
-    "SESSION_STRING_2",
-    # Add more session strings as needed
+    "BQFgymkAIm-auno6Zrvp6Dya59D5eV2-1FvVtBsB-55TJd2Hy9gSOSO2-So6xVPztmCljtfJuI4lagV-5Di4rzKblAKsrzA7hPVZpjZzOAW4NKNZE8qSFhZ4rOEqryK2JXFIxNINvfifnSqWw08tf5W2Nytm30361EvjS7DWeaSfAwaHxK6SGpwx4FRpwub2f0_NgpJFE8Wx7BQxNLSjEzCfSfHq2IRYXFNgyG9IBfze__uL-6QogfGHeUeXmCqY37w2ldNYHonpdmjUsxtNLd_YaocP0BJ3Z-FIShe4viahSANMWgsVlkljV1mHdPypkD_GsVicOvudFKiJGEFFCRlnxmhTVQAAAAGvVNXGAA",
+    "BQFgymkAVTMxSUj6LsU_3n5Drp2A6Qxrf_1CdOpNK2SqvQHYFehSmu1rzZr6ZeWZVffbquBPV-orsQKh01ydXX4yNfPybJP8b1PVZnNf8NEOb-vxw0ATay79muzQCUH_EZj8m0LTakEfphuq7nXK2cU6ravdGwDTqQtcez8Pqnz0II8W8PRlZwkUHzXe2VFKuLC3gLamhRyV6AT6sPANyU9Pb2mx9VC_s0RiPpbFQy_1KNmqgHWt5ZNxK267LcreOVTmF6lW1K9iSZfPE78YFIR9scwq01Pf9Cv8Ali8fi5TVj8ago2M0m9WjibLCZls15uYmYR5YTNZoM1aZQrWfTzuRfa_7AAAAAG3U1eFAA",
+    "BQFgymkAezjS5jiJSx-AZ07UHv-1BVM9I3Y3d45o3RVg2-4jICfCZfGcvI-81BmxryzDhrb936ytuX0VElw3OdHbuNE0hIHkUHuIFjIF-8Gscilk1whOjz7d28rkS-2Hg-Hrd6GS__HKfwHVrey8JVpC4iKEtU1iXMxfmBnlVtqqVZvpPL9NNX9hLOhUtdOiC9bzZ3qv8mlzP7SMZ3UjJuPz1MATHgUuyKv4QzCplmPgWpFGlfFUwnkPsKffeu2S7MaDhteRopEB1eQtEahHb_hPLw97vm0_GR02zLHGZdkeXKtltIE7J7q5h2NpLyT7OBO94DFuHq3KgrA6oLM540q22vxYDQAAAAGuvoCcAA"
 ]
 
 # List of report reasons
